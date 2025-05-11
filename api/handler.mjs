20edd69d474da1eca5bc4b021f5e8e9a13f0b10a
fenixcyc <fenixcyc@fenixcyc_student.21_school.ru>
2025-05-11 12:39:26 +0700

    image

--- api/handler.mjs
+++ api/handler.mjs
@@ -8,23 +8,23 @@ export const config = {
   // https://ai.google.dev/gemini-api/docs/available-regions#available_regions
   // Vercel Edge Network Regions
   // https://vercel.com/docs/edge-network/regions#region-list
-  regions: [
-    "arn1",
-    "bom1",
-    "cdg1",
-    "cle1",
-    "cpt1",
-    "dub1",
-    "fra1",
-    "gru1",
+  regions: ["iad1"]
+  //  "arn1",
+  //  "bom1",
+  //  "cdg1",
+  //  "cle1",
+  //  "cpt1",
+  //  "dub1",
+  //  "fra1",
+  //  "gru1",
     //"hkg1",
-    "hnd1",
-    "iad1",
-    "icn1",
-    "kix1",
-    "pdx1",
-    "sfo1",
-    "sin1",
-    "syd1",
-  ],
+  //  "hnd1",
+  //  "iad1"
+  //  "icn1",
+  //  "kix1",
+  //  "pdx1",
+  //  "sfo1",
+  //  "sin1",
+  //  "syd1",
+  //],
 };
